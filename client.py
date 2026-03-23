f15db8f5e5d4433d82d37f3199421aa791f12610
EC2 Default User <ec2-user@ip-172-31-63-230.ec2.internal>
2026-03-23 19:45:26 +0000

    Implementa operações de soma, sub, e mult entre servidor e cliente

--- client.py
+++ client.py
@@ -3,7 +3,14 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+
+print("Escolha uma operação: ")
+operation = input("[0] Adicao | [1] Subtracao | [2] Mutiplicar: ")
+op1 = input("Digite o primeiro numero: ")
+op2 = input("Digite o segund numero: ")
+
+s.send(f"[{operation},{op1},{op2}]".encode())  # send some data
+print("Sent operation 0, and operands 1,2 to server")
 data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+print ("Resultado recebido do servidor: " + bytes.decode(data))            # print the result
 s.close()               # close the connection
--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 from socket  import *
 from constCS import * #-
+import math
+import json
 
 s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
+s.bind(('0.0.0.0', PORT))  #-
 s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  
+  dados = bytes.decode(data)
+  array = json.loads(data)
+  operation = array[0]
+  op1 = array[1]
+  op2 = array[2]
+
+  res = None
+
+  if(operation == 0): res = op1 + op2
+  elif(operation == 1): res = op1 - op2
+  elif(operation == 2): res = op1 * op2
+  print("Data recieved from client: " + bytes.decode(data))
+  conn.send(str.encode(str(res)))
 conn.close()               # close the connection
